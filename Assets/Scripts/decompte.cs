10f712b2a5d9dc3e2f864bd2b4cdcee8356d9da1
TheSnowly <thesnowly18@gmail.com>
2023-02-04 02:01:05 +0100

    Button activate when timer

--- Assets/Scripts/decompte.cs
+++ Assets/Scripts/decompte.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class decompte : MonoBehaviour
 {
+
+    [SerializeField] private GameObject Button1;
+    [SerializeField] private GameObject Button2;
 
     private IEnumerator Wait(float WaitTime) {
         Debug.Log("3");
@@ -16,12 +20,16 @@ public class decompte : MonoBehaviour
         //3
         yield return new WaitForSeconds(WaitTime);
         Debug.Log("GOOOOOO");
+        Button1.SetActive(true);
+        Button2.SetActive(true);
         //GOOOOOO
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        Button1.SetActive(false);
+        Button2.SetActive(false);
         StartCoroutine(Wait(1F));
     }
 
